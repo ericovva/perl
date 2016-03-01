bf8da706f56cfc7212b02e665b4129613edc8d32
System Administrator <root@MacBook-Air-Erik.local>
2016-03-02 01:48:31 +0300

    hm2

--- hm1/d.pl
+++ hm1/d.pl
@@ -1,14 +1,14 @@
 #!/usr/bin/perl -naF':'
-BEGIN{
-	use Data::Dumper; 
-	use Data::Printer; 
+BEGIN{ 
+	use Data::Dumper;
+	use Data::Printer;
 	$k=0; @ar = (); 
 	$ans="";
 }   
 	$j=0; 
 	@ar2=();  
 	foreach (@F) { 
-		push (@ar2,"$_");  
+		push (@ar2,$_);  
 		if ("$_" =~ /^[+-]?\d+$/ && $_ > 10) { 
 			$ans=$ans . "i: $k j: $j --- $_\n" 
 		} 
