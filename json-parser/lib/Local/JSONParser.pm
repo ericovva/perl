9782c3d81ab25ad79ce4df1f5808b1fa624d8091
System Administrator <root@MacBook-Air-Erik.local>
2016-03-23 18:48:08 +0300

    tabs in json-parser

--- json-parser/lib/Local/JSONParser.pm
+++ json-parser/lib/Local/JSONParser.pm
@@ -34,122 +34,114 @@ sub parse_json {
             push (@ref_arr,\%new_hash);
             push(@state,"hash");
         }
-            if (/\G("[^"]+"\s*:)/gc) {
-                my $ys = $1;
+        if (/\G("[^"]+"\s*:)/gc) {
+            my $ys = $1;
             $ys =~ s{\s}{}g;
             $ys =~ s{[":]}{}g;
-                ##print "$ys 1\n";
-                push (@keys,$ys);
+            ##print "$ys 1\n";
+            push (@keys,$ys);
+        }
+        elsif (m{\G("(?:\\"|[^"])*")}gc){
+            ##print "$1 2\n";
+            my $tmp = $1;
+            chop($tmp);
+            $tmp = reverse($tmp);
+            chop($tmp);
+            $tmp = reverse($tmp);
+            $tmp =~ s{\\"}{"}g;
+            $tmp =~ s{\\t}{\t}g;
+            #$tmp =~ s{\\u(\d+)}{$1}g;
+            $tmp =~ s{\\u(\d+)}{  chr(hex($1)) }ge;
+            if ($state[$#state] eq "hash"){
+                ${$ref_arr[$#ref_arr]}{$keys[$#keys]} = $tmp;
+                pop (@keys);
             }
-            elsif (m{\G("(?:\\"|[^"])*")}gc){
-                ##print "$1 2\n";
-                my $tmp = $1;
-                chop($tmp);
-                $tmp = reverse($tmp);
-                chop($tmp);
-                $tmp = reverse($tmp);
-                $tmp =~ s{\\"}{"}g;
-                    $tmp =~ s{\\t}{\t}g;
-                    #$tmp =~ s{\\u(\d+)}{$1}g;
-                    $tmp =~ s{\\u(\d+)}{  chr(hex($1)) }ge;
+            elsif ($state[$#state] eq "ar"){
+                push(@{$ref_arr[$#ref_arr]},$tmp);
+            }    
+        }
+        elsif (m{\G(["])([^\1]*)\1\]}gc){
+            ##print "$2 3\n";
+            my $tmp = $2;
+            $tmp =~ s{\\"}{"}g;
+            $tmp =~ s{\\t}{\t}g;
+            if ($state[$#state] eq "ar"){
+                push(@{$ref_arr[$#ref_arr]},$tmp);
+            }
+            if ($state[$#state] ne "ar") { die "ne ok"; }
+            pop (@state);
+            $balance--;
+            if ($#ref_arr>0){#p @ref_arr;
+                if ($state[$#state] eq "hash"){
+                    ${$ref_arr[$#ref_arr - 1]}{$keys[$#keys]} = $ref_arr[$#ref_arr];
+                    pop (@keys);
+                }
+                elsif ($state[$#state] eq "ar"){
+                    push(@{$ref_arr[$#ref_arr - 1]},$ref_arr[$#ref_arr]);
+                }
+                pop (@ref_arr);
+            }
+        }
+        elsif (m{\G(["])([^\1]*)\1\s}gc){
+            ##print "$2 4\n";
+            my $tmp = $2;
+            $tmp =~ s{\\"}{"}g;
+            $tmp =~ s{\\t}{\t}g;
+            if ($state[$#state] eq "hash"){
+                ${$ref_arr[$#ref_arr]}{$keys[$#keys]} = $tmp;
+                pop (@keys);
+            }
+            elsif ($state[$#state] eq "ar"){
+                push(@{$ref_arr[$#ref_arr]},$tmp);
+            }
+        }
+        if (/\G(-?\d+.?\d*)/gc){
+            ##print "$1\n";
+            if ($state[$#state] eq "hash"){
+                ${$ref_arr[$#ref_arr]}{$keys[$#keys]} = $1 + 0;
+                pop (@keys);
+            }
+            elsif ($state[$#state] eq "ar"){
+                push(@{$ref_arr[$#ref_arr]},$1 + 0);
+            }
+        }
+        if (/\G(\])/gc){
+            $balance--;
+            if ($state[$#state] ne "ar") { die "ne ok"; }
+                pop(@state);
+                if ($#ref_arr>0){#p @ref_arr;
                     if ($state[$#state] eq "hash"){
-                        ${$ref_arr[$#ref_arr]}{$keys[$#keys]} = $tmp;
+                        ${$ref_arr[$#ref_arr - 1]}{$keys[$#keys]} = $ref_arr[$#ref_arr];
                         pop (@keys);
                     }
                     elsif ($state[$#state] eq "ar"){
-                        push(@{$ref_arr[$#ref_arr]},$tmp);
+                        push(@{$ref_arr[$#ref_arr - 1]},$ref_arr[$#ref_arr]);
                     }
-                    
+                    pop (@ref_arr);
                 }
-                elsif (m{\G(["])([^\1]*)\1\]}gc){
-                    ##print "$2 3\n";
-                    my $tmp = $2;
-                    $tmp =~ s{\\"}{"}g;
-                        $tmp =~ s{\\t}{\t}g;
-                        if ($state[$#state] eq "ar"){
-                            push(@{$ref_arr[$#ref_arr]},$tmp);
-                        }
-                        if ($state[$#state] ne "ar") { die "ne ok"; }
-                        pop (@state);
-                        $balance--;
-                        if ($#ref_arr>0){#p @ref_arr;
-                            if ($state[$#state] eq "hash"){
-                                ${$ref_arr[$#ref_arr - 1]}{$keys[$#keys]} = $ref_arr[$#ref_arr];
-                                pop (@keys);
-                            }
-                            elsif ($state[$#state] eq "ar"){
-                                push(@{$ref_arr[$#ref_arr - 1]},$ref_arr[$#ref_arr]);
-                            }
-                            pop (@ref_arr);
-                        }
-                    }
-                    elsif (m{\G(["])([^\1]*)\1\s}gc){
-                        ##print "$2 4\n";
-                        my $tmp = $2;
-                        $tmp =~ s{\\"}{"}g;
-                            $tmp =~ s{\\t}{\t}g;
-                            if ($state[$#state] eq "hash"){
-                                ${$ref_arr[$#ref_arr]}{$keys[$#keys]} = $tmp;
-                                pop (@keys);
-                            }
-                            elsif ($state[$#state] eq "ar"){
-                                push(@{$ref_arr[$#ref_arr]},$tmp);
-                            }
-                        }
-                        if (/\G(-?\d+.?\d*)/gc){
-                            ##print "$1\n";
-                            if ($state[$#state] eq "hash"){
-                                ${$ref_arr[$#ref_arr]}{$keys[$#keys]} = $1 + 0;
-                                pop (@keys);
-                            }
-                            elsif ($state[$#state] eq "ar"){
-                                push(@{$ref_arr[$#ref_arr]},$1 + 0);
-                            }
-                        }
-                        if (/\G(\])/gc){
-                            $balance--;
-                            if ($state[$#state] ne "ar") { die "ne ok"; }
-                            pop(@state);
-                            if ($#ref_arr>0){#p @ref_arr;
-                                if ($state[$#state] eq "hash"){
-                                    ${$ref_arr[$#ref_arr - 1]}{$keys[$#keys]} = $ref_arr[$#ref_arr];
-                                    pop (@keys);
-                                }
-                                elsif ($state[$#state] eq "ar"){
-                                    push(@{$ref_arr[$#ref_arr - 1]},$ref_arr[$#ref_arr]);
-                                }
-                                pop (@ref_arr);
-                            }
-                            #print "$1\n";
-                            
-                        }
-                        if (/\G(\})/gc){
-                            $balance--;
-                            if ($state[$#state] ne "hash") { die "ne ok"; }
-                            pop(@state);
-                            if ($#ref_arr>0){
-                                #p @ref_arr;
-                                if ($state[$#state] eq "hash"){
-                                    ${$ref_arr[$#ref_arr - 1]}{$keys[$#keys]} = $ref_arr[$#ref_arr];
-                                    pop (@keys);
-                                }
-                                elsif ($state[$#state] eq "ar"){
-                                    push(@{$ref_arr[$#ref_arr - 1]},$ref_arr[$#ref_arr]);
-                                }
-                                pop (@ref_arr);
-                            }
-                            
-                            #print "$1\n";
-                            
-                        }  
+                #print "$1\n";                    
+        }
+        if (/\G(\})/gc){
+            $balance--;
+            if ($state[$#state] ne "hash") { die "ne ok"; }
+            pop(@state);
+            if ($#ref_arr>0){
+                #p @ref_arr;
+                if ($state[$#state] eq "hash"){
+                    ${$ref_arr[$#ref_arr - 1]}{$keys[$#keys]} = $ref_arr[$#ref_arr];
+                    pop (@keys);
                 }
-                #print "$source\n";
-                if (!$balance) {return $ref_arr[0];}
-                else {die "No balance"};
-                
-                
-                
-                
-            }
+                elsif ($state[$#state] eq "ar"){
+                    push(@{$ref_arr[$#ref_arr - 1]},$ref_arr[$#ref_arr]);
+                }
+                pop (@ref_arr);
+            } 
+            #print "$1\n";            
+        }  
+    }
+    #print "$source\n";
+    if (!$balance) {return $ref_arr[0];}
+    else {die "No balance"};
+}
             
-            1;
+1;
